437902f1f41d752ba2590d25b985838129932cd0
Michael Heap <m@michaelheap.com>
2018-02-12 00:08:30 +0000

    Transcribe audio and add it to a ticket

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\TicketEntry;
 use Illuminate\Http\Request;
 use Log;
+use App\Ticket;
 
 class WebhookController extends Controller
 {
@@ -23,12 +24,64 @@ class WebhookController extends Controller
             [
                 'action' => 'talk',
                 'text' => $ticket->content
+            ],
+            [
+                'action' => 'talk',
+                'text' => 'To add a reply, please leave a message after the beep, then press the pound key',
+                'voiceName' => 'Brian'
+            ],
+            [
+                "action" => "record",
+                "endOnKey" => "#",
+                "beepStart" => true
             ]
         ]);
     }
 
     public function event(Request $request) {
-        Log::info('Call event', $request->all());
+        $params = $request->all();
+        Log::info('Call event', $params);
+        if (isset($params['recording_url'])) {
+            $voiceResponse = $this->transcribeRecording($params['recording_url']);
+
+            $ticket = Ticket::all()->last();
+            $user = $ticket->subscribedUsers()->first();
+
+            $entry = new TicketEntry([
+                'content' => $voiceResponse,
+                'channel' => 'voice',
+            ]);
+
+            $entry->user()->associate($user);
+            $entry->ticket()->associate($ticket);
+            $entry->save();
+        }
         return response('', 204);
+    }
+
+    public function transcribeRecording($recordingUrl) {
+        $audio = \Nexmo::get($recordingUrl)->getBody();
+
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => 'https://stream.watsonplatform.net/'
+        ]);
+
+        $transcriptionResponse = $client->request('POST', 'speech-to-text/api/v1/recognize', [
+            'auth' => ['username', 'password'],
+            'headers' => [
+                'Content-Type' => 'audio/mpeg',
+            ],
+            'body' => $audio
+        ]);
+
+        $transcription = json_decode($transcriptionResponse->getBody());
+
+        $voiceResponse = '';
+        foreach ($transcription->results as $result) {
+            $voiceResponse .= $result->alternatives[0]->transcript.' ';
+        }
+
+        return $voiceResponse;
+
     }
 }
