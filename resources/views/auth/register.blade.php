990177738a64040a6c6e9ff244bcccfb729f54c8
Lorna Jane Mitchell <lorna@lornajane.net>
2019-07-04 10:53:01 +0100

    Better form element naming fixes #2

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -62,9 +62,9 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('phone_number') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Phone Number</label>
+                            <label for="phone-number" class="col-md-4 control-label">Phone Number</label>
                             <div class="col-md-6">
-                                <input id="name" type="tel" class="form-control" name="phone_number" value="{{ old('phone_number') }}" required autofocus>
+                                <input id="phone-number" type="tel" class="form-control" name="phone_number" value="{{ old('phone_number') }}" required autofocus>
 
                                 @if ($errors->has('phone_number'))
                                     <span class="help-block">
